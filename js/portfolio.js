0b6f29eb2768c24424c0846b3a937a2c19d11b75
ManasiPagare <ManasiPagare@github.com>
2020-11-05 23:59:28 +1300

    Initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,19 @@
+
+document.getElementsById("ui-ux").addEventListener("click",function()
+
+{
+	var results=document.getElementsByClassName("ui-ux-images");
+	if(results.style.display=="none")
+	{
+		results.style.display="block";
+	
+	}
+	
+	else
+		
+		{
+		results.style.display="none";
+		}
+
+})
+		
